9b4ecdf00dab093785fcf2edd6bee7f6647ac72d
Dan Mapes <156453470+dmapesdtac@users.noreply.github.com>
2025-07-03 12:38:58 -0400

    Adding BRICKS

--- Source/BrickSpace/Private/HandSelector.cpp
+++ Source/BrickSpace/Private/HandSelector.cpp
@@ -5,7 +5,7 @@
 
 #include "Vodget.h"
 
-UHandSelector::UHandSelector()
+UHandSelector::UHandSelector() 
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
