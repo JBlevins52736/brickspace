efd4f690c8a73ef0b742a90629b25def56e29e6f
Jasen Blevins <135083567+JBlevins52736@users.noreply.github.com>
2025-08-07 11:52:20 -0400

    smart anchor

--- Source/BrickSpace/Private/SmartAnchor.cpp
+++ Source/BrickSpace/Private/SmartAnchor.cpp
@@ -1,34 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "SmartAnchor.h"
+#include "GameFramework/Actor.h"
 
-// Sets default values for this component's properties
 USmartAnchor::USmartAnchor()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
-
-// Called when the game starts
 void USmartAnchor::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
-	// ...
+void USmartAnchor::CreateAnchor(const bool Value)
+{
+	Marker->SetWorldLocation(controller->GetComponentLocation());
+
+
+}
+
+void USmartAnchor::OnCreateAnchorComplete(EOculusXRAnchorResult::Type Result, UOculusXRAnchorComponent* Anchor)
+{
 	
 }
-
-
-// Called every frame
-void USmartAnchor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
--- Source/BrickSpace/Public/SmartAnchor.h
+++ Source/BrickSpace/Public/SmartAnchor.h
@@ -1,28 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "OculusXRSpatialAnchorComponent.h"
+#include "OculusXRAnchorBPFunctionLibrary.h"
 #include "SmartAnchor.generated.h"
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BRICKSPACE_API USmartAnchor : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
+public:
 	USmartAnchor();
 
+	// Call this from a button to create an anchor
+	UFUNCTION(BlueprintCallable, Category = "SmartAnchor")
+	void CreateAnchor(const bool Value);
+	UPROPERTY(BlueprintReadWrite)
+	USceneComponent* Marker;
+	UPROPERTY(BlueprintReadWrite)
+	USceneComponent* controller;
+
+
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+private:
+	// Callback when anchor creation finishes
+	UFUNCTION()
+	void OnCreateAnchorComplete(EOculusXRAnchorResult::Type Result, UOculusXRAnchorComponent* Anchor);
 };
