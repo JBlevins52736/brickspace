71389b02a57dceb5be65e963eb9ad2a009a9a7e1
Dan Mapes <156453470+dmapesdtac@users.noreply.github.com>
2025-09-06 08:48:36 -0400

    Move replication test to a plugin

---
+++ Plugins/ReplicationTest/Source/ReplicationTest/Public/Dummy.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Dummy.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class REPLICATIONTEST_API ADummy : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADummy();
+
+	UFUNCTION(Server, Reliable)
+	void Server_ChangeColor(AActor* TargetActor);
+
+	UPROPERTY(ReplicatedUsing = OnRep_Color)
+	FLinearColor colorVec;
+
+	UFUNCTION()
+	virtual void OnRep_Color();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	UStaticMeshComponent* CubeMeshComponent;
+	UMaterialInstanceDynamic* DynamicMaterialInstance;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+};
